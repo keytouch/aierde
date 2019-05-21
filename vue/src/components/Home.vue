f106d3ffe2e2edff5570cc6689c0934695d21e6d
keytouch <20770013+keytouch@users.noreply.github.com>
2019-05-21 21:32:16 +0800

    Initial commit

---
+++ vue/src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="home">
+<router-view/>
+<m>
+
+</div>
+</template>
+<script>
+</script>
+<style>
+</style>
---
+++ vue/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path:'/home',
+      children:[
+        {
+          path: '/page1',
+          name: 'HelloWorld',
+          component: HelloWorld
+        },
+      ]
+    }
+  ]
+})
